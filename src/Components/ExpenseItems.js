40eec775ae3bb21fc694e79cf0ae5bc61d045818
krishna <krishnadj7431@gmail.com>
2023-04-24 16:43:18 +0530

    expense component is created

---
+++ src/Components/ExpenseItems.js
@@ -0,0 +1,16 @@
+
+import './ExpenseItems.css';
+
+function ExpenseItems(){
+   return (
+         <div className="expense-items">
+            <div>March 12th 2023</div>
+            <div className='expense-description'>
+                  <h2>Car Serives</h2>
+                  <p>$200</p>
+            </div>
+         </div>
+         )
+}
+
+export default ExpenseItems
