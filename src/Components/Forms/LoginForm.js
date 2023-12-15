5908cacf37d3e647b9477399f66eff0f2723cf4b
krishna <krishnadj7431@gmail.com>
2023-12-15 10:21:07 +0530

    Login form ui is customised

--- src/Components/Forms/LoginForm.js
+++ src/Components/Forms/LoginForm.js
@@ -10,45 +10,53 @@ import {
 
 export default function LoginForm() {
   return (
-    <form>
-      <MDBInput className='mb-4' type='email' id='form2Example1' label='Email address' />
-      <MDBInput className='mb-4' type='password' id='form2Example2' label='Password' />
+    <div className='col-sm-5 centre offset-4 my-5'>
+      <form>
+        <h2>Welcome back! login now...</h2>
+        <div className='text-center my-5'>
 
-      <MDBRow className='mb-4'>
-        <MDBCol className='d-flex justify-content-center'>
-          <MDBCheckbox id='form2Example3' label='Remember me' defaultChecked />
-        </MDBCol>
-        <MDBCol>
-          <a href='#!'>Forgot password?</a>
-        </MDBCol>
-      </MDBRow>
+          <MDBBtn floating color="secondary" className='mx-1'>
+            <MDBIcon fab icon='facebook-f' />
+          </MDBBtn>
 
-      <MDBBtn type='submit' className='mb-4' block>
-        Sign in
-      </MDBBtn>
+          <MDBBtn floating color="secondary" className='mx-1'>
+            <MDBIcon fab icon='google' />
+          </MDBBtn>
 
-      <div className='text-center'>
+          <MDBBtn floating color="secondary" className='mx-1'>
+            <MDBIcon fab icon='twitter' />
+          </MDBBtn>
+
+          <MDBBtn floating color="secondary" className='mx-1'>
+            <MDBIcon fab icon='github' />
+          </MDBBtn>
+        </div>
+        
+
+
+        <MDBInput className='mb-4' type='email' id='form2Example1' label='Email address' />
+        <MDBInput className='mb-4' type='password' id='form2Example2' label='Password' />
+
+        <MDBRow className='mb-4'>
+          <MDBCol className='d-flex justify-content-center'>
+            <MDBCheckbox id='form2Example3' label='Remember me' defaultChecked />
+          </MDBCol>
+          <MDBCol>
+            <a href='#!'>Forgot password?</a>
+          </MDBCol>
+        </MDBRow>
+
+        
+
+        <MDBBtn type='submit' className='mb-4' block>
+          Sign in
+        </MDBBtn>
         <p>
-          Not a member? <a href='#!'>Register</a>
-        </p>
-        <p>or sign up with:</p>
+            Not a member? <a href='#!'>Register</a>
+          </p>
 
-        <MDBBtn floating color="secondary" className='mx-1'>
-          <MDBIcon fab icon='facebook-f' />
-        </MDBBtn>
-
-        <MDBBtn floating color="secondary" className='mx-1'>
-          <MDBIcon fab icon='google' />
-        </MDBBtn>
-
-        <MDBBtn floating color="secondary" className='mx-1'>
-          <MDBIcon fab icon='twitter' />
-        </MDBBtn>
-
-        <MDBBtn floating color="secondary" className='mx-1'>
-          <MDBIcon fab icon='github' />
-        </MDBBtn>
-      </div>
-    </form>
+        
+      </form>
+    </div>
   );
 }
